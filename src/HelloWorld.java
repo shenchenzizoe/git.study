fba6ad1b2e4e50ad57abac26fa70e35fa9edae34
shenchenzi <shenchenzizoe@outlook.com>
2022-05-14 18:36:44 +0800

    try to directly commit

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -2,7 +2,9 @@ public class HelloWorld {
     private int name;
     private int phoneNumber;
     //can I directly commit?
-
+    //yes
+    //can I directly push?
+    //no,have to commit first
     public HelloWorld(int name, int phoneNumber) {
         this.name = name;
         this.phoneNumber = phoneNumber;
